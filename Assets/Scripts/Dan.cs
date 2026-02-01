889c0f656ca945870842fb8aec6be107054ef8dd
Hoanglong958 <hoanglongh911@gmail.com>
2026-02-02 00:05:14 +0700

    demo part 3

--- Assets/Scripts/Dan.cs
+++ Assets/Scripts/Dan.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 public class Dan : MonoBehaviour
 {
-public GameObject bulletPrefabs;
-public float shootingInterval;
-private float lastBulletTime;
-void Update()
-{
-if (Input.GetMouseButton(0))
-{
-if (Time.time - lastBulletTime >
-shootingInterval)
-{
-ShootBullet();
-lastBulletTime = Time.time;
-}
-}
-}
+    public GameObject bulletPrefabs;
+    public float shootingInterval;
+    
+    // 1. Thêm biến để điều chỉnh khoảng cách vị trí bắn
+    public Vector3 bulletOffset; 
+    
+    private float lastBulletTime;
 
-private void ShootBullet()
-{
-Instantiate(bulletPrefabs, transform.position, transform.rotation);
-}
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time - lastBulletTime > shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
+
+    private void ShootBullet()
+    {
+        // 2. Cộng thêm bulletOffset vào vị trí hiện tại (transform.position)
+        Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
+    }
 }
